a291554e498af09b366ed91f0da16f10fd54bff7
Haoye <haoye@instituto.org>
2026-04-14 10:50:37 +0200

    Configuración de ViewController

--- tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/config/WebConfiguration.java
+++ tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/config/WebConfiguration.java
@@ -1,4 +1,13 @@
 package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.web.config;
 
-public class WebConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/condiciones-uso").setViewName("terms");
+    }
 }

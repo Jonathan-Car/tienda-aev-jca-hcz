51c1a0b1206493ae394bcd4064e89092014d1c8d
Angel <angel.escobar1@educa.madrid.org>
2026-04-14 10:50:31 +0200

    Primeros avances en el repositorio de categoría así como en su implementación

--- tienda-aev-jca-hcz-common/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/common/repositories/CategoriaRepositoryImpl.java
+++ tienda-aev-jca-hcz-common/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/common/repositories/CategoriaRepositoryImpl.java
@@ -1,4 +1,35 @@
 package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.repositories;
 
-public class CategoriaRepositoryImpl {
+import es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.entities.Categoria;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class CategoriaRepositoryImpl implements ICategoriaRepository {
+
+    private Map<Long, Categoria> categorias = new HashMap<>();
+
+
+    @Override
+    public List<Categoria> findAll() {
+
+        return new ArrayList<Categoria>(categorias.values());
+    }
+
+    @Override
+    public Categoria findById(Long id) {
+
+        return categorias.get(id);
+    }
+
+    @Override
+    public void save(Categoria categoria) {
+
+        categorias.put(categoria.getId(), categoria);
+    }
+
 }

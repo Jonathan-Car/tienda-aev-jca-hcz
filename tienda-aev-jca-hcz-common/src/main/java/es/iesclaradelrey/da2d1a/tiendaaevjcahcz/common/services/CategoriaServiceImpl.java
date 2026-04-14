8402cf8786c02d855a3d3a5738f5fa8fedb4af9c
Haoye Chen <haoye@instituto.org>
2026-04-14 22:20:39 +0200

    Creación de servicio de categorías

--- tienda-aev-jca-hcz-common/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/common/services/CategoriaServiceImpl.java
+++ tienda-aev-jca-hcz-common/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/common/services/CategoriaServiceImpl.java
@@ -1,4 +1,32 @@
 package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.services;
 
-public class CategoriaServiceImpl {
+import es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.entities.Categoria;
+import es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.repositories.ICategoriaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriaServiceImpl implements ICategoriaService {
+
+    private final ICategoriaRepository categoriaRepository;
+
+    public CategoriaServiceImpl(ICategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    @Override
+    public List<Categoria> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Categoria findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(Categoria categoria) {
+        categoriaRepository.save(categoria);
+    }
 }
--- tienda-aev-jca-hcz-common/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/common/services/ICategoriaService.java
+++ tienda-aev-jca-hcz-common/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/common/services/ICategoriaService.java
@@ -1,4 +1,10 @@
 package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.services;
 
+import es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.entities.Categoria;
+import java.util.List;
+
 public interface ICategoriaService {
+    List<Categoria> findAll();
+    Categoria findById(Long id);
+    void save(Categoria categoria);
 }

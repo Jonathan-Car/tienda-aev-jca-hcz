ce0954e8c37b4e52943408e6dacd32f3b951f62c
Jonathan-Car <jonathancardenas904@gmail.com>
2026-04-15 11:49:12 +0200

    Controlador de las categorias

--- tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/controllers/CategoriaController.java
+++ tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/controllers/CategoriaController.java
@@ -1,23 +1,38 @@
 package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.web.controllers;
 
+import es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.entities.Categoria;
+import org.springframework.ui.Model;
 import es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.services.ICategoriaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Collection;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
 @Controller
-@GetMapping("/categorias") //url
-public class CategoriaController {
+@RequestMapping("/categorias") //url
+public class CategoriaController{
     //inyeccion de la interfaz
     private final ICategoriaService categoriaService;
     //constructor
     public CategoriaController(ICategoriaService categoriaService) {
         this.categoriaService = categoriaService;
-
     }
     @GetMapping({"/", ""})
     public String listado(Model model) {
-        //pedir las categorias
+        // pedimos las categoría y las guardamos en una lista
+        List<Categoria> categorias = categoriaService.findAll();
+        model.addAttribute("categorias", categorias);
+        //devolvemos el html de la lista
+        return "categorias/list";
+    }
+    @GetMapping("/{id}")
+    public String detalle(@PathVariable("id") Long id, Model model) {
+        //buscamos la categoria por ID
+        Categoria categoria = categoriaService.findById(id);
+        // añadimos al modelo la categoria
+        model.addAttribute("categoria", categoria);
+        //devolvemos el html de detalle
+        return "categorias/detail";
     }
 }

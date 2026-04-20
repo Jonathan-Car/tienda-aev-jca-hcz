fe0d7915a7950a67726b667b06b882f5f7d74219
Angel <angel.escobar1@educa.madrid.org>
2026-04-20 13:07:29 +0200

    Modificación de la capa de presentación.

--- tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/models/CategoriaViewModel.java
+++ tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/models/CategoriaViewModel.java
@@ -1,4 +1,29 @@
 package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.web.models;
 
 public class CategoriaViewModel {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private String imagen;
+
+    public CategoriaViewModel() {}
+
+    public CategoriaViewModel(Long id, String nombre, String descripcion, String imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    public String getImagen() { return imagen; }
+    public void setImagen(String imagen) { this.imagen = imagen; }
 }

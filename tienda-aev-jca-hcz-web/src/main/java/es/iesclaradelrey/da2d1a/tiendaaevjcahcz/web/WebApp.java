9088a0f5f2abc2546c98e0c958d5d9effee9275b
Angel <angel.escobar1@educa.madrid.org>
2026-04-20 13:25:51 +0200

    Añadiendo estilo a las páginas.

--- tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/WebApp.java
+++ tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/WebApp.java
@@ -2,13 +2,10 @@ package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication(scanBasePackages = {
-        // Escaneo para encontrar controladores y vistas
-        "es.iesclaradelrey.da2d1a.tiendaaevjcahcz.web",
-
-        // Escanea el common para que encuentre los @Service y el @Repository
-        "es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common"
-})
+@SpringBootApplication(
+        scanBasePackages = "es.iesclaradelrey.da2d1a.tiendaaevjcahcz",
+        exclude = { org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class }
+)
 public class WebApp {
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);

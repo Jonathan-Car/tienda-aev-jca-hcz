da66c44a12d7f002426fe70bde710799a272c84a
Angel <angel.escobar1@educa.madrid.org>
2026-04-22 14:00:29 +0200

    Creación de clase Marca y su repositorio, ajuste del archivo properties.

---
+++ tienda-aev-jca-hcz-web/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO categorias (id, nombre, descripcion, imagen) VALUES
+    (1, 'Portátiles', 'Ordenadores portátiles para trabajo, gaming y uso doméstico.', 'portatilDell.png'),
+    (2, 'Periféricos', 'Teclados, ratones, monitores y otros accesorios esenciales.', 'perifericos.png'),
+    (3, 'Componentes', 'Placas base, procesadores, memorias RAM y tarjetas gráficas.', 'tablets.png'),
+    (4, 'Almacenamiento', 'Discos duros HDD, SSD y memorias USB de alta velocidad.', 'ssd1tb.png');

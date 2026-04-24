3cd7c0575dfc40fd1696e7e8b90afd0b1d91dde5
Jonathan-Car <jonathancardenas904@gmail.com>
2026-04-24 10:29:51 +0200

    control del formulario en CategoriaAdminCOntroller

--- tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/controllers/admin/CategoriaAdminController.java
+++ tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/controllers/admin/CategoriaAdminController.java
@@ -1,10 +1,10 @@
 package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.web.controllers.admin;
 
+import es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.entities.Categoria;
 import es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.services.ICategoriaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/admin/categorias")
@@ -15,14 +15,56 @@ public class CategoriaAdminController {
         this.categoriaService = categoriaService;
     }
 
-    @GetMapping("")
+    @GetMapping("/")
     public String listaRedirigidas() {
         return "redirect:/admin/categorias";
     }
+
     @GetMapping("")
     public String lista(Model model) {
         model.addAttribute("categorias", categoriaService.findAll());
         return "admin/categorias/lista";
     }
+    @GetMapping("/nueva")
+    public String nuevaCategoria(Model model) {
+        model.addAttribute("categoria", new Categoria());
+        return "admin/categorias/formulario";
+    }
 
+    @GetMapping("/{id}/editar")
+    public String editarCategoria(@PathVariable Long id, Model model) {
+        Categoria categoria = categoriaService.findById(id);
+        model.addAttribute("categoria", categoria);
+        return "admin/categorias/formulario";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(@ModelAttribute("categoria") Categoria categoria ,Model model) {
+        try{
+            categoriaService.save(categoria);
+            return "redirect:/admin/categorias";
+        }catch(Exception e){
+            model.addAttribute("error", "Error al guardar: " + e.getMessage());
+            return "admin/categorias/formulario";
+        }
+    }
+
+    @GetMapping("/{id}/borrar")
+    public String confirmarBorrado(@PathVariable Long id, Model model) {
+        Categoria categoria = categoriaService.findById(id);
+        model.addAttribute("categoria", categoria);
+        return "admin/categorias/borrar";
+    }
+
+    @PostMapping("/{id}/borrar")
+    public String eliminar(@PathVariable Long id, Model model) {
+        try {
+            categoriaService.deleteById(id);
+            return "redirect:/admin/categorias";
+        } catch (Exception e) {
+            model.addAttribute("error", "Error al eliminar: " + e.getMessage());
+            model.addAttribute("categoria", categoriaService.findById(id));
+            return "admin/categorias/borrar";
+        }
+    }
 }
--- tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/controllers/admin/MarcaAdminController.java
+++ tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/controllers/admin/MarcaAdminController.java
@@ -1,4 +1,17 @@
 package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.web.controllers.admin;
 
+import es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.repositories.IMarcaRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin/marcas")
 public class MarcaAdminController {
+    private IMarcaRepository marcaRepository;
+
+    @GetMapping("/")
+    public String marcaRedirigida() {
+        return "redirect:/admin/marcas";
+    }
 }

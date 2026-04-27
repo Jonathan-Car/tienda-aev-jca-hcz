a727376fece19bc604c18ff8a4126a577327953d
Jonathan-Car <jonathancardenas904@gmail.com>
2026-04-27 16:56:17 +0200

    Avances de los controladores de Admin de Marca y Producto.

--- tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/controllers/admin/MarcaAdminController.java
+++ tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/controllers/admin/MarcaAdminController.java
@@ -1,9 +1,10 @@
 package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.web.controllers.admin;
 
+import es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.entities.Marca;
 import es.iesclaradelrey.da2d1a.tiendaaevjcahcz.common.repositories.IMarcaRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("admin/marcas")
@@ -13,5 +14,19 @@ public class MarcaAdminController {
     @GetMapping("/")
     public String marcaRedirigida() {
         return "redirect:/admin/marcas";
+    }
+    @GetMapping("/nueva")
+    public String nuevaMarca(Model model) {
+        model.addAttribute("marca", marcaService.findALl());
+        return "admin/marcas/formulario";
+    }
+    @PostMapping("/{id}/editar")
+    public String editarMarca(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("marca", marcaService.findById(id));
+        return "admin/marcas/formulario";
+    }
+    @PostMapping("/guardar")
+    public String guardarMarca(@ModelAttribute("marca") Model model, Marca marca) {
+
     }
 }
--- tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/controllers/admin/ProductoAdminController.java
+++ tienda-aev-jca-hcz-web/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/web/controllers/admin/ProductoAdminController.java
@@ -1,4 +1,32 @@
 package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.web.controllers.admin;
 
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+ @RequestMapping("/admin/productos")
 public class ProductoAdminController {
+    private final IProductoService productoService;
+
+    public ProductoAdminController(IProductoService productoService, IMarcaService marcaService){
+        this.productoService = productoService;
+    }
+
+    @GetMapping("/")
+    public String redireccion(){
+        return "redirect:/admin/productos";
+    }
+    @GetMapping("")
+    public String lista(Model model){
+        model.addAttribute("productos", productoService.findAll());
+        return "admin/productos/lista";
+    }
+    @GetMapping("/nuevo")
+    public String nuevoProducto(Model model){
+        model.addAttribute("producto", new Producto());
+        return "admin/productos/formulario";
+    }
+
 }

5fa57330901d60e57a45bb0ef4e6c35676167ca3
Angel <angel.escobar1@educa.madrid.org>
2026-05-07 09:02:27 +0200

    Solucionando módulo API dios sabrá porque

---
+++ tienda-aev-jca-hcz-api/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/api/config/HelloWorld.java
@@ -0,0 +1,8 @@
+package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.api.config;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+
+    }
+}

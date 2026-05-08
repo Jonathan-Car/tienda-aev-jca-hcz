e26967cd04591f45f1cf5051bc01b19d06805bd0
Jonathan-Car <jonathancardenas904@gmail.com>
2026-05-08 12:59:17 +0200

    Corrección de errores en el modulo api.

--- tienda-aev-jca-hcz-api/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/api/config/SeguridadConfig.java
+++ tienda-aev-jca-hcz-api/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/api/config/SeguridadConfig.java
@@ -10,8 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -43,9 +41,5 @@ public class SeguridadConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
-    }
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
--- tienda-aev-jca-hcz-security/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/security/config/SecurityBeansConfig.java
+++ tienda-aev-jca-hcz-security/src/main/java/es/iesclaradelrey/da2d1a/tiendaaevjcahcz/security/config/SecurityBeansConfig.java
@@ -2,14 +2,14 @@ package es.iesclaradelrey.da2d1a.tiendaaevjcahcz.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class SecurityBeansConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
+        return NoOpPasswordEncoder.getInstance();
     }
 }
